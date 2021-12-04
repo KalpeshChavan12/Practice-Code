69151150716ca7c81b106d7fdb8bcb82e1f94d0f
Kalpesh Chavan <18593210+KalpeshChavan12@users.noreply.github.com>
2021-12-04 18:06:49 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,6 +3,8 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
+#include <math.h>
+
 //global variables
 #define MAX_STACK 1000
 float numbers[MAX_STACK];
@@ -28,17 +30,47 @@ char pop_op()
 {
     return op[to--];
 }
+
+bool is_unary(char s)
+{
+    switch (s)
+    {
+        case 'S': // Trignometric function
+            return true;
+        case 'T':
+            return true;
+        case 'C':
+            return true;
+        case 'O': //COt
+            return true;
+        case 'E': // SEC
+            return true;
+        case 'I': // SIN
+            return true;
+    }
+
+    return false;
+
+}
+
 //evaluating the expression
 float infix_eval(float numbers[MAX_STACK], char op[MAX_STACK])
 {
     float x, y;
     char ope;
-    //taking first two operands
-    x = pop_num();
-    y = pop_num();
+    
     //taking the operators between them
     ope = pop_op();
-    //executing the operation
+
+
+
+    //taking first two operands
+    x = pop_num();
+    if (!is_unary(ope))
+    {
+        y = pop_num();
+    }
+        //executing the operation
     switch (ope)
     {
         case '+':
@@ -55,14 +87,27 @@ float infix_eval(float numbers[MAX_STACK], char op[MAX_STACK])
             }
             else
                 return y / x;
+        case 'S' :
+            return sin(x);
+        case 'C':
+            return cos(x);
+        case 'T':
+            return tan(x);
+        case 'E':
+            return 1/cos(x);
+        case 'I':
+            return 1/sin(x);
+        case 'O':
+            return 1/tan(x);
     }
     return 0;
 }
 //function to check whether the character is an operator or not 
 int is_operator(char ch)
 {
-    return(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+    return(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch == 'S' || ch == 'T' || ch == 'C' || ch == 'I' || ch == 'O' || ch == 'E');
 }
+
 //the precedence of the operators
 int precedence(char c)
 {
@@ -78,6 +123,18 @@ int precedence(char c)
             return 2;
         case '^':
             return 3;
+        case 'S': // Trignometric function
+            return 4;
+        case 'T':
+            return 4;
+        case 'C':
+            return 4;
+        case 'O': //COt
+            return 4;
+        case 'E': // SEC
+            return 4;
+        case 'I': // SIN
+            return 4;
     }
     return -1;
 }
@@ -151,7 +208,8 @@ float eval(char exp[20])
 //main function
 int main()
 {
-    char exp[50] = "1/2";
+    char exp[50] = "10*S(10)-5*T(8+7*9)";
+    //char exp[50] = "T(1/2)";
     //taking the expression to evaluate
     printf("Enter the infix expression to evaluate: ");
     //scanf("%s",exp);
